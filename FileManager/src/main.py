6d26097a06311d6e943c363df775218b0fca1bd6
Orion <timur.mambetov2001@mail.ru>
2026-05-11 15:24:24 +0300

    Испарвление уязвимостей для Task6

--- FileManager/src/main.py
+++ FileManager/src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+import bleach
 from schemas import UserCreate
 
 app = FastAPI()
@@ -12,11 +14,31 @@ def registration(user: UserCreate):
 templates = Jinja2Templates(directory="templates")
 comments_store = []
 
+ALLOWED_TAGS = ['b', 'i', 'u', 'em', 'strong']
+
+def sanitize_comment(raw_text: str) -> str:
+    return bleach.clean(
+        raw_text,
+        tags=ALLOWED_TAGS,
+        attributes={},
+        strip=True,
+        strip_comments=True
+    )
+
+class CSPMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        response.headers["Content-Security-Policy"] = "default-src 'self'; script-src 'self'; style-src 'self';"
+        return response
+
+app.add_middleware(CSPMiddleware)
+
 @app.get("/comments", response_class=HTMLResponse)
 async def get_comments(request: Request):
     return templates.TemplateResponse("comments.html", {"request": request, "comments": comments_store})
 
 @app.post("/comments", response_class=HTMLResponse)
 async def post_comment(request: Request, text: str = Form(...)):
-    comments_store.append(text)
+    clean_text = sanitize_comment(text)
+    comments_store.append(clean_text)
     return templates.TemplateResponse("comments.html", {"request": request, "comments": comments_store})
